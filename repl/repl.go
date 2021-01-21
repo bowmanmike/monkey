55884da5d2c97a644fbc607bdbdc2cee16baf76f
Mike Bowman <mbowman2@gmail.com>
2021-01-21 11:26:37 -0500

    Tweak repl

--- repl/repl.go
+++ repl/repl.go
@@ -15,7 +15,7 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Printf(PROMPT)
+		fmt.Fprintf(out, PROMPT)
 
 		scanned := scanner.Scan()
 		if !scanned {
