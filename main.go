4705cbd04f59911a7b727b73e0044edb56ae0132
Mike Bowman <mbowman2@gmail.com>
2021-01-21 11:06:26 -0500

    Add repl

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/bowmanmike/monkey/repl"
 )
 
 func main() {
-	fmt.Println("Hello!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey Programming Language!\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
 }
