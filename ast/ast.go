97956a9f45d80e557b069d6654d44f4c960f3a71
Mike Bowman <mbowman2@gmail.com>
2021-01-24 15:57:12 -0500

    Work on ast

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/bowmanmike/monkey/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -23,4 +25,25 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 	return p.Statements[0].TokenLiteral()
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
