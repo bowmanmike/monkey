c758eac39b9b9bfe10827f887dc27b9cef48595d
Mike Bowman <mbowman2@gmail.com>
2021-02-03 21:06:06 -0500

    Remove debug statement

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,7 +1,6 @@
 package lexer
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/bowmanmike/monkey/token"
@@ -113,9 +112,6 @@ func TestNextToken(t *testing.T) {
 	for i, tt := range tests {
 		tok := l.NextToken()
 
-		if i == 8 {
-			fmt.Printf("%+v", tok)
-		}
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - Type wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
 		}
