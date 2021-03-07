fac00368ecc4b90ae4549c52c37ac0989b762cd4
Mike Bowman <mbowman2@gmail.com>
2021-03-07 18:32:35 -0500

    Start evaluating and applying functions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -36,6 +36,10 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		env.Set(node.Name.Value, val)
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
 
 	// Expressions
 	case *ast.IntegerLiteral:
@@ -60,6 +64,11 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return evalInfixExpression(node.Operator, left, right)
 	case *ast.IfExpression:
 		return evalIfExpression(node, env)
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
 	}
 
 	return nil
